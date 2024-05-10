31ba37c7b7309a606ac34cbf09bdb0eb9ff49a17
Marek Stefaniak <madgotten@wp.pl>
2024-05-10 18:38:18 +0200

    Add Firebase authentication context provider

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
+import { AuthProvider } from "./context/AuthProvider";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<>Home</>} />
+      <AuthProvider>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<>Home</>} />
 
-          {/* Must be always last route */}
-          <Route path="*" element={<>404 page not found</>} />
-        </Route>
-      </Routes>
+            {/* Must be always last route */}
+            <Route path="*" element={<>404 page not found</>} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
