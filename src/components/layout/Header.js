76b812fd0f90dc2e4149866690ee21185f9f6321
Agata <agatajacyk.webdeveloper@gmail.com>
2024-05-12 23:43:28 +0200

    header update
    Header is on the page

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,5 +1,25 @@
+import "./Header.css";
 const Header = () => {
-    {/* Header */}
+  return (
+    <div class="nav">
+      <input type="checkbox" id="nav-check" />
+      <div class="nav-header"></div>
+      <div class="nav-btn">
+        <label for="nav-check">
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
+      </div>
+      <div class="nav-links">
+        <a>Strona główna</a>
+        <a>Głosowanie tygodnia</a>
+        <a>Głosowanie miesiąca</a>
+        <div id="indicator"></div>
+        <button>Logowanie</button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
