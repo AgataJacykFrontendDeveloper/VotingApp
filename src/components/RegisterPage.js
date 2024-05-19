9844c1a5919706cc426a7c98a0b6462f04515f4c
Marek Stefaniak <madgotten@wp.pl>
2024-05-19 12:54:47 +0200

    Merge origin/main into login

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -1,5 +1,86 @@
+import AuthContext from "../context/AuthProvider";
+import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import "./LoginPage.css";
+import { SocialAuth } from "./auth/SocialAuth";
+
 const RegisterPage = () => {
-    {/* RegisterPage */}
-};
+  const user = useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState("");
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (password !== confirmPassword) {
+        throw new Error("Hasła muszą być takie same");
+      }
+      const userCredential = await user.signupUser(
+        email,
+        password
+      );
+      console.log("Poprawnie zarejestrowano użytkownika:", userCredential.user);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="auth-container container d-flex justify-content-center">
+      <div className="auth-panel">
+        <h1 className="auth-panel-h1">Zarejestruj się</h1>
+        {/* Przyciski Google, FB, Twitter */}
+        <SocialAuth />
+
+        <form onSubmit={onSubmit} className="auth-form d-flex flex-column gap-3">
+          <div className="auth form-group">
+            <label htmlFor="email">e-mail</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              required
+            />
+          </div>
+          <div className="auth form-group">
+            <label htmlFor="password">hasło</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              required
+            />
+          </div>
+          <div className="auth form-group">
+            <label htmlFor="confirmPassword">potwórz hasło</label>
+            <input
+              type="password"
+              value={confirmPassword}
+              autoComplete="off"
+              onChange={(event) => {
+                setconfirmPassword(event.target.value);
+              }}
+              required
+            />
+          </div>
+          <div>
+            <input type="checkbox"/>
+            <label htmlFor="newsletter">Zapisz się do newslettera</label>
+          </div>
+          <div>
+            <input required type="checkbox"/>
+            <label htmlFor="terms">Akceptuj warunki użytkowania</label>
+          </div>
+          <div className="d-flex justify-content-center mt-3">
+            <button type="submit" className="btn v2 mx-5">Załóż konto</button>
+          </div>
+        </form>
+        <Link to="/login">
+          <p>« Powrót do logowania</p>
+        </Link>
+      </div>
+    </div>
+  );
+
+      };
 
 export default RegisterPage;
--- src/components/UserPanel.js
+++ src/components/UserPanel.js
@@ -1,14 +1,66 @@
 import "./UserPanel.css";
+import React, { useState } from "react";
 
 const UserPanel = () => {
+  const [activeButton, setActiveButton] = useState(null);
+
   return (
     <div className="container">
       <div className="left-menu">
-        <button class=" votes">Głosy oddane dzisiaj</button>
-        <button class=" user-info">Informacje podstawowe</button>
-        <button class="settings">Dodatkowe ustawienia</button>
+        <button
+          className={`votes ${activeButton === "votes" ? "active" : ""}`}
+          onClick={() => setActiveButton("votes")}
+        >
+          Oddane głosy
+        </button>
+
+        <button
+          className={`user-info ${
+            activeButton === "user-info" ? "active" : ""
+          }`}
+          onClick={() => setActiveButton("user-info")}
+        >
+          Informacje podstawowe
+        </button>
+
+        <button
+          className={`settings ${activeButton === "settings" ? "active" : ""}`}
+          onClick={() => setActiveButton("settings")}
+        >
+          Dodatkowe ustawienia
+        </button>
       </div>
-      <div className="right-menu"></div>
+      <div className="right-menu">
+        {activeButton === "votes" && <div className="votes-content">Głosy</div>}
+        {activeButton === "user-info" && (
+          <div className="user-content">
+            <p className="user-name">Użytkownik</p>
+            <p className="user-data user-data-name">
+              E-mail
+              <input className="input-name" type="text" />
+            </p>
+            <p className="user-data user-data-password">
+              Hasło
+              <input className="input-password" type="text" />
+            </p>
+            <input
+              class="check-box-1"
+              placeholder
+              className="x"
+              type="checkbox"
+            />
+            <span>Zapis do newslettera</span>
+
+            <p class="delete-account">&#x2716; usuń konto</p>
+          </div>
+        )}
+
+        {activeButton === "settings" && (
+          <div className="additional-settings-content">
+            Dodatkowe ustawienia
+          </div>
+        )}
+      </div>
     </div>
   );
 };
