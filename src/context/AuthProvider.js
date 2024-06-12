6e1c670642a021b885802b04e2cd1fd35d59fd42
Marek Stefaniak <madgotten@wp.pl>
2024-06-12 21:25:54 +0200

    Check if user is an admin

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,6 +1,7 @@
 import { useState, useEffect, createContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { auth } from "../firebase/firebase.js";
+import { auth, db } from "../firebase/firebase.js";
+import { getDoc, doc } from "firebase/firestore";
 import {
   signInWithRedirect,
   getRedirectResult,
@@ -14,7 +15,7 @@ import {
 } from "firebase/auth";
 
 export const AuthContext = createContext();
-auth.languageCode = 'pl';
+auth.languageCode = "pl";
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
@@ -37,9 +38,11 @@ export const AuthProvider = ({ children }) => {
     "auth/email-already-in-use": "Na ten adres e-mail jest już założone konto",
     "auth/internal-error": "Błąd po stronie serwera. Spróbuj ponownie później",
     "auth/invalid-password": "Za słabe hasło. Musi być bardziej złożone",
-    "auth/maximum-user-count-exceeded": "Osiągnieto maksymalną liczbę użytkowników. Poinformuj administratora",
+    "auth/maximum-user-count-exceeded":
+      "Osiągnieto maksymalną liczbę użytkowników. Poinformuj administratora",
     "auth/uid-already-exists": "UID użytkownika już istnieje",
-    "auth/user-not-found": "Nie znaleziono użytkownika o takim adresie e-mail. Sprawdź poprawność wpisanego e-maila"
+    "auth/user-not-found":
+      "Nie znaleziono użytkownika o takim adresie e-mail. Sprawdź poprawność wpisanego e-maila",
   };
 
   function checkErrorMessage(e) {
@@ -47,9 +50,23 @@ export const AuthProvider = ({ children }) => {
     return errorMessages[errorCode] || errorMessages["auth/unexpected-error"];
   }
 
+  const checkIsAdmin = async (user) => {
+    try {
+      const userDoc = await getDoc(doc(db, "users", user.uid));
+      if (userDoc.exists()) {
+        setUser({ ...user, isAdmin: userDoc.data().isAdmin });
+      } else {
+        setUser({ ...user, isAdmin: false });
+      }
+    } catch (error) {
+      console.warn(error);
+    }
+  };
+
   useEffect(() => {
     const currentUser = auth.onAuthStateChanged((authUser) => {
       setUser(authUser);
+      checkIsAdmin(authUser);
     });
 
     return currentUser;
@@ -125,16 +142,17 @@ export const AuthProvider = ({ children }) => {
   const forgotPassword = async (email) => {
     setIsLoading(true);
     try {
-        await sendPasswordResetEmail(auth, email)
+      await sendPasswordResetEmail(auth, email);
       // Jeżeli w konsoli Firebase jest włączony email enumeration protection to zawsze zwracany jest taki sam request więc odpowiedź zawsze będzie taka sama
-        return { message: "Jeżeli w bazie danych znajduje się ten e-mail, została na niego wysłana wiadomość z linkiem do resetowania hasła" };
-      }
-      catch (error) {
-        throw new Error(checkErrorMessage(error));
-      }
-      finally {
-        setIsLoading(false);
-      }
+      return {
+        message:
+          "Jeżeli w bazie danych znajduje się ten e-mail, została na niego wysłana wiadomość z linkiem do resetowania hasła",
+      };
+    } catch (error) {
+      throw new Error(checkErrorMessage(error));
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const signOut = async () => {
