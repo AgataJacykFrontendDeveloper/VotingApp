796dffb5c2236d931d259f213f38a5bcbea0eeca
Dominika <pimmortals@gmail.com>
2024-07-23 18:01:57 +0200

    active tabs

--- src/components/AdministratorPanel.js
+++ src/components/AdministratorPanel.js
@@ -1,146 +1,134 @@
-import React from "react";
-
+import React, { useState } from "react";
 import "../dist/css/vendors.bundle.css";
 import "../dist/css/app.bundle.css";
 import "../dist/css/skins/skin-master.css";
 
-
 const AdministratorPanel = () => {
+  const [activeTab, setActiveTab] = useState("oddaneGlosy");
+
+  const handleTabClick = (tabName) => {
+    setActiveTab(tabName);
+  };
+
   return (
     <>
-      <body class="mod-bg-1 mod-nav-link ">
-        <div class="page-wrapper">
-          <div class="page-inner">
-            <aside class="page-sidebar">
-              <nav id="js-primary-nav" class="primary-nav" role="navigation">
-                <ul id="js-nav-menu" class="nav-menu">
-                  <li class="active open">
+      <div className="mod-bg-1 mod-nav-link">
+        <div className="page-wrapper">
+          <div className="page-inner">
+            <aside className="page-sidebar">
+              <nav id="js-primary-nav" className="primary-nav" role="navigation">
+                <ul id="js-nav-menu" className="nav-menu">
+                  <li className={activeTab === "oddaneGlosy" ? "active open" : ""}>
                     <a
                       href="#"
-                      title="Application Intel"
-                      data-filter-tags="application intel"
+                      title="Oddane głosy"
+                      onClick={() => handleTabClick("oddaneGlosy")}
                     >
-                      <i class="fal fa-info-circle"></i>
-                      <span
-                        class="nav-link-text"
-                        data-i18n="nav.application_intel"
-                      >
-                        Oddane głosy
-                      </span>
+                      <i className="fal fa-info-circle"></i>
+                      <span className="nav-link-text">Oddane głosy</span>
                     </a>
                   </li>
-                  <li>
+                  <li className={activeTab === "aktywneGlosowania" ? "" : ""}>
                     <a
                       href="#"
-                      title="Theme Settings"
-                      data-filter-tags="theme settings"
+                      title="Aktywne głosowania"
+                      onClick={() => handleTabClick("aktywneGlosowania")}
                     >
-                      <i class="fal fa-cog"></i>
-                      <span
-                        class="nav-link-text"
-                        data-i18n="nav.theme_settings"
-                      >
-                        Aktywne głosowania
-                      </span>
+                      <i className="fal fa-cog"></i>
+                      <span className="nav-link-text">Aktywne głosowania</span>
                     </a>
                   </li>
-                  <li>
+                  <li className={activeTab === "uzytkownicy" ? "active open" : ""}>
                     <a
                       href="#"
-                      title="Documentation"
-                      data-filter-tags="documentation"
+                      title="Użytkownicy"
+                      onClick={() => handleTabClick("uzytkownicy")}
                     >
-                      <i class="fal fa-book"></i>
-                      <span class="nav-link-text" data-i18n="nav.documentation">
-                        Użytkownicy
-                      </span>
+                      <i className="fal fa-book"></i>
+                      <span className="nav-link-text">Użytkownicy</span>
                     </a>
                   </li>
-                  <li>
+                  <li className={activeTab === "ustawienia" ? "active open" : ""}>
                     <a
                       href="#"
-                      title="Documentation"
-                      data-filter-tags="documentation"
+                      title="Ustawienia"
+                      onClick={() => handleTabClick("ustawienia")}
                     >
-                      <i class="fal fa-book"></i>
-                      <span class="nav-link-text" data-i18n="nav.documentation">
-                        Ustawienia
-                      </span>
+                      <i className="fal fa-book"></i>
+                      <span className="nav-link-text">Ustawienia</span>
                     </a>
                   </li>
-                  <li>
+                  <li className={activeTab === "nowaListaUtworow" ? "active open" : ""}>
                     <a
                       href="#"
-                      title="Documentation"
-                      data-filter-tags="documentation"
+                      title="Nowa lista utworów"
+                      onClick={() => handleTabClick("nowaListaUtworow")}
                     >
-                      <i class="fal fa-book"></i>
-                      <span class="nav-link-text" data-i18n="nav.documentation">
-                        Nowa lista utworów
-                      </span>
+                      <i className="fal fa-book"></i>
+                      <span className="nav-link-text">Nowa lista utworów</span>
                     </a>
                   </li>
                 </ul>
-                <div class="filter-message js-filter-message bg-success-600"></div>
+                <div className="filter-message js-filter-message bg-success-600"></div>
               </nav>
             </aside>
 
-            <div class="page-content-wrapper">
-              <header class="page-header" role="banner">
-                <div class="hidden-md-down dropdown-icon-menu position-relative">
+            <div className="page-content-wrapper">
+              <header className="page-header" role="banner">
+                <div className="hidden-md-down dropdown-icon-menu position-relative">
                   <a
                     href="#"
-                    class="header-btn btn js-waves-off"
+                    className="header-btn btn js-waves-off"
                     data-action="toggle"
                     data-class="nav-function-hidden"
                     title="Hide Navigation"
                   >
-                    <i class="ni ni-menu"></i>
+                    <i className="ni ni-menu"></i>
                   </a>
                   <ul>
                     <li>
                       <a
                         href="#"
-                        class="btn js-waves-off"
+                        className="btn js-waves-off"
                         data-action="toggle"
                         data-class="nav-function-minify"
                         title="Minify Navigation"
                       >
-                        <i class="ni ni-minify-nav"></i>
+                        <i className="ni ni-minify-nav"></i>
                       </a>
                     </li>
                     <li>
                       <a
                         href="#"
-                        class="btn js-waves-off"
+                        className="btn js-waves-off"
                         data-action="toggle"
                         data-class="nav-function-fixed"
                         title="Lock Navigation"
                       >
-                        <i class="ni ni-lock-nav"></i>
+                        <i className="ni ni-lock-nav"></i>
                       </a>
                     </li>
                   </ul>
                 </div>
 
-                <div class="hidden-lg-up">
+                <div className="hidden-lg-up">
                   <a
                     href="#"
-                    class="header-btn btn press-scale-down"
+                    className="header-btn btn press-scale-down"
                     data-action="toggle"
                     data-class="mobile-nav-on"
                   >
-                    <i class="ni ni-menu"></i>
+                    <i className="ni ni-menu"></i>
                   </a>
                 </div>
 
-                <div class="ml-auto d-flex">
+                <div className="ml-auto d-flex">
                   <div>
                     <a
                       href="#"
                       data-toggle="dropdown"
                       title="drlantern@gotbootstrap.com"
-                      class="header-icon d-flex align-items-center justify-content-center ml-2"
+                      className="header-icon d-flex align-items-center justify-content-center ml-2"
                     >
                       Wyloguj
                     </a>
@@ -148,38 +136,59 @@ const AdministratorPanel = () => {
                 </div>
               </header>
 
-              <main id="js-page-content" role="main" class="page-content">
-                <div class="subheader">
-                  <h1 class="subheader-title">Oddane głosy</h1>
+              <main id="js-page-content" role="main" className="page-content">
+                <div className="subheader">
+                  <h1 className="subheader-title">
+                    {activeTab === "oddaneGlosy" && "Oddane głosy"}
+                    {activeTab === "aktywneGlosowania" && "Aktywne głosowania"}
+                    {activeTab === "uzytkownicy" && "Użytkownicy"}
+                    {activeTab === "ustawienia" && "Ustawienia"}
+                    {activeTab === "nowaListaUtworow" && "Nowa lista utworów"}
+                  </h1>
                 </div>
-                <div class="fs-lg fw-300 p-5 bg-white border-faded rounded mb-g">
-                  <h2>Privacy</h2>
-                  <p class="mb-g">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Sunt quis iste aperiam laborum? Laborum aspernatur
-                    voluptatum quaerat voluptate quod tempore tenetur rerum
-                    explicabo dolor culpa tempora obcaecati, exercitationem quia
-                    perferendis.
-                  </p>
-                  <h3>Cookies and other similar technologies</h3>
-                  <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Aut ab voluptates ipsam aliquam unde illo porro quia
-                    repellat tempore ratione? Magni, inventore quod! Sunt unde,
-                    numquam eaque quod iste alias?
-                  </p>
+                <div className="fs-lg fw-300 p-5 bg-white border-faded rounded mb-g">
+                  {activeTab === "oddaneGlosy" && (
+                    <div>
+                      <h2>Oddane głosy</h2>
+                      <p>xxx</p>
+                    </div>
+                  )}
+                  {activeTab === "aktywneGlosowania" && (
+                    <div>
+                      <h2>Aktywne głosowania</h2>
+                      <p>xxx</p>
+                    </div>
+                  )}
+                  {activeTab === "uzytkownicy" && (
+                    <div>
+                      <h2>Użytkownicy</h2>
+                      <p>xxx</p>
+                    </div>
+                  )}
+                  {activeTab === "ustawienia" && (
+                    <div>
+                      <h2>Ustawienia</h2>
+                      <p>xxx</p>
+                    </div>
+                  )}
+                  {activeTab === "nowaListaUtworow" && (
+                    <div>
+                      <h2>Nowa lista utworów</h2>
+                      <p>xxx</p>
+                    </div>
+                  )}
                 </div>
               </main>
 
               <div
-                class="page-content-overlay"
+                className="page-content-overlay"
                 data-action="toggle"
                 data-class="mobile-nav-on"
               ></div>
             </div>
           </div>
         </div>
-      </body>
+      </div>
     </>
   );
 };
