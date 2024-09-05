f6d2581c72b088d37f72daf0154d165cfe68d4f8
Marek Stefaniak <madgotten@wp.pl>
2024-09-05 17:59:57 +0200

    Fix Modal to automatically close on route change

--- src/context/ModalProvider.js
+++ src/context/ModalProvider.js
@@ -1,19 +1,30 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
+import { useLocation } from "react-router-dom";
 import Modal from "../components/overlays/Modal";
 
 const ModalContext = createContext();
 
 export const ModalProvider = ({ children }) => {
   const [modalContent, setModalContent] = useState(null);
+  const currentLocation = useRef(null);
+  let location = useLocation().pathname;
 
   const openModal = (content) => {
     setModalContent(content);
+    currentLocation.current = location;
   };
 
   const closeModal = () => {
     setModalContent(null);
+    currentLocation.current = null;
   };
+
+  useEffect(() => {
+    if (currentLocation.current && currentLocation.current !== location) {
+      closeModal();
+    }
+  }, [location]);
 
   return (
     <ModalContext.Provider value={{ openModal, closeModal }}>
