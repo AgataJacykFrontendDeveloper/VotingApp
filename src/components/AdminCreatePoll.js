e2be2baec7943529554d96114c91194232933cb3
Marek Stefaniak <madgotten@wp.pl>
2024-09-27 01:42:34 +0200

    Add feat to add multiple songs to a new poll

--- src/components/AdminCreatePoll.js
+++ src/components/AdminCreatePoll.js
@@ -1,5 +1,7 @@
-import { doc, setDoc, getDoc } from "firebase/firestore";
+import { useState, useContext } from "react";
+import { doc, setDoc, addDoc, getDoc, collection } from "firebase/firestore";
 import { db } from "../firebase/firebase";
+import AlertContext from "../context/AlertProvider";
 
 const createSlug = (title) => {
   return title
@@ -10,14 +12,35 @@ const createSlug = (title) => {
     .replace(/\s+/g, "-"); // Replace whitespaces with hyphen
 };
 
-const AdminCreatePoll = (addAlert) => {
+const AdminCreatePoll = () => {
+  const { addAlert } = useContext(AlertContext);
+  const [songs, setSongs] = useState([]);
+
+  const handleSongAdd = () => {
+    setSongs([...songs, { artist: "", genre: "", title: "", url: "" }]);
+  };
+
+  const handleSongDelete = (i) => {
+    const updatedSongs = songs.filter((_, index) => index !== i);
+    setSongs(updatedSongs);
+  };
+
+  const handleSongChange = (e, i) => {
+    const { name, value } = e.target;
+    const updatedSongs = [...songs];
+    updatedSongs[i][name] = value;
+    setSongs(updatedSongs);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
-    const form = {};
-    for (const [key, value] of formData.entries()) {
-      form[key] = value;
-    }
+    const form = {
+      title: formData.get("title"),
+      type: formData.get("type"),
+      start_at: formData.get("start_at"),
+      end_at: formData.get("end_at"),
+    };
 
     if (form.title.length < 3) {
       return addAlert("Nazwa głosowania musi zawierać co najmniej 3 znaki.");
@@ -29,16 +52,27 @@ const AdminCreatePoll = (addAlert) => {
       );
     }
 
+    if (songs.length === 0) {
+      return addAlert("Musisz dodać co najmniej jeden utwór.");
+    }
+
     const slug = createSlug(form.title);
 
     try {
+      // TODO: Change later to transaction with batched writes
       const docRef = doc(db, "polls", slug);
       const docSnap = await getDoc(docRef);
       if (docSnap.exists()) {
         return addAlert("Głosowanie o takiej nazwie już istnieje.");
       } else {
         await setDoc(docRef, form);
+
+        const songsRef = collection(docRef, "songs");
+        for (let song of songs) {
+          await addDoc(songsRef, song);
+        }
         e.target.reset();
+        setSongs([]);
         return addAlert("Głosowanie zostało utworzone.", "success");
       }
     } catch (error) {
@@ -96,6 +130,96 @@ const AdminCreatePoll = (addAlert) => {
           className="form-control"
           required
         />
+
+        <div>
+          <h4 className="ml-n2 mt-2">Lista utworów</h4>
+          {songs.map((song, i) => (
+            <div key={i} className="d-flex flex-column gap-1 mb-3">
+              <div className="d-flex flex-row gap-2">
+                <h5 className="fw-semibold my-auto">Utwór {i + 1}</h5>
+                <button
+                  className="border-0 p-1 bg-danger rounded h-100"
+                  onClick={() => handleSongDelete(i)}
+                >
+                  <svg
+                    className="text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18 17.94 6M18 18 6.06 6"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <label htmlFor={`song-artist-${i}`} className="form-label">
+                Artysta
+              </label>
+              <input
+                type="text"
+                name="artist"
+                id={`song-artist-${i}`}
+                className="form-control"
+                value={song.artist}
+                onChange={(e) => handleSongChange(e, i)}
+                required
+              />
+
+              <label htmlFor={`song-genre-${i}`} className="form-label">
+                Gatunek
+              </label>
+              <input
+                type="text"
+                name="genre"
+                id={`song-genre-${i}`}
+                className="form-control"
+                value={song.genre}
+                onChange={(e) => handleSongChange(e, i)}
+                required
+              />
+
+              <label htmlFor={`song-title-${i}`} className="form-label">
+                Tytuł
+              </label>
+              <input
+                type="text"
+                name="title"
+                id={`song-title-${i}`}
+                className="form-control"
+                value={song.title}
+                onChange={(e) => handleSongChange(e, i)}
+                required
+              />
+
+              <label htmlFor={`song-url-${i}`} className="form-label">
+                URL
+              </label>
+              <input
+                type="url"
+                name="url"
+                id={`song-url-${i}`}
+                className="form-control"
+                value={song.url}
+                onChange={(e) => handleSongChange(e, i)}
+                required
+              />
+            </div>
+          ))}
+          <button
+            type="button"
+            onClick={handleSongAdd}
+            className="btn-cyan w-fit"
+          >
+            Dodaj utwór
+          </button>
+        </div>
+
         <button type="submit" className="btn-cyan w-fit">
           Utwórz głosowanie
         </button>
